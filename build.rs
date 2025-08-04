a90d595122cdab98612555687ae876569f9e1daf
aitzazisstuffed <aitzazimtiaz855@gmail.com>
2025-08-05 00:46:21 +0500

    fix: syntax error at panic statement

--- build.rs
+++ build.rs
@@ -1,15 +1,21 @@
 //! Skirk - A port scanner written in Rust.
 //!
 //! This script acts as a binder to connect C code to Rust
+
 use std::env;
 use std::path::Path;
 
 fn main() {
-  let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-  let object_path = Path::new(&manifest_dir).join("bin/skirk.o");
-  if !object_path.exists() {
-    panic!("\x1b[34m\A core component of building skirk was not found. Aborting.\n\If this installation was from cargo, please report this issue to the repository at https://github.com/aitzazisstuffed/skirk/issues.\\x1b[0m");
-  }
-  println!("cargo:rustc-link-arg={}", object_path.display());
-  println!("cargo:rerun-if-changed=bin/skirk.o");
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let object_path = Path::new(&manifest_dir).join("bin/skirk.o");
+
+    if !object_path.exists() {
+        panic!("\x1b[34m\
+A core component of building skirk was not found. Aborting.\n\
+If this installation was from cargo, please report this issue to the repository at https://github.com/aitzazisstuffed/skirk/issues.\
+\x1b[0m");
+    }
+
+    println!("cargo:rustc-link-arg={}", object_path.display());
+    println!("cargo:rerun-if-changed=bin/skirk.o");
 }
