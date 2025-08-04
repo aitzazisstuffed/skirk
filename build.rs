2406f5cfb03ae5fda7baa92888770365c27dbf23
aitzazisstuffed <aitzazimtiaz855@gmail.com>
2025-08-05 00:44:49 +0500

    fix: remove second panic statement and concatenate

--- build.rs
+++ build.rs
@@ -8,8 +8,7 @@ fn main() {
   let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
   let object_path = Path::new(&manifest_dir).join("bin/skirk.o");
   if !object_path.exists() {
-    panic!("\x1b[34mA Core component of building skirk was not found. Aborting.\x1b[0m");
-    panic!("\x1b[34mA If this installation was from cargo, please report this issue to the repository at https://github.com/aitzazisstuffed/skirk/issues.\x1b[0m");
+    panic!("\x1b[34m\A core component of building skirk was not found. Aborting.\n\If this installation was from cargo, please report this issue to the repository at https://github.com/aitzazisstuffed/skirk/issues.\\x1b[0m");
   }
   println!("cargo:rustc-link-arg={}", object_path.display());
   println!("cargo:rerun-if-changed=bin/skirk.o");
